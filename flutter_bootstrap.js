36a3bdc86b422ca50670f6220b72f9ce302b4a56
gruvw <gruvw@users.noreply.github.com>
2025-05-20 10:51:56 +0000

    deploy: d91d4ae6cfff6ca24868feb9f677d442ec951f6d

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "888375628"
+    serviceWorkerVersion: "4144473018"
   }
 });
